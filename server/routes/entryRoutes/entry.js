253012ce83f5db978dfcb1c3f2c2748e9f1b4a5b
irakoze yves <irakozeyves1@gmail.com>
2019-10-17 18:41:25 +0200

    ft(update-entry) update an dairy entry [#168967468]

--- server/routes/entryRoutes/entry.js
+++ server/routes/entryRoutes/entry.js
@@ -1,9 +1,9 @@
 /* eslint-disable eol-last */
 import { Router } from 'express';
 import { verifyToken } from '../../middleware/token.middleware';
-import { entry } from '../../controllers/Entry/addentry';
+import { entry, update } from '../../controllers/Entry/addentry';
 
 const router = Router();
 router.post('/entries', verifyToken, entry);
-
+router.patch('/entries/:entryId', verifyToken, update);
 export default router;
